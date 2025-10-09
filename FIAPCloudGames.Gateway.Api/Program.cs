0f81d9675356b551f1365f9bdde02feb78528db4
PeterHSS <pedro.henrique.ismart@gmail.com>
2025-10-08 23:21:14 -0300

    feat: add gateway project with reverse proxy setup
    Introduced a new .NET 8.0 web application project,
    `FIAPCloudGames.Gateway.Api`, to act as a reverse proxy for handling
    requests to user, order, and game services. The project includes
    support for JWT-based authentication and role-based authorization,
    with routes and clusters configured using YARP.
    
    Added a Visual Studio solution file (`FIAPCloudGames.Gateway.sln`) to
    organize the project structure. Configured `appsettings.json` to
    define logging, connection strings, JWT settings, and reverse proxy
    routes and clusters. Introduced `appsettings.Development.json` for
    development-specific logging configurations.
    
    Updated `Program.cs` to configure the application pipeline, including
    reverse proxy setup, authentication, and authorization middleware.
    Defined an "AdminOnly" authorization policy for securing specific
    routes. Added `Roles.cs` to centralize role definitions.
    
    Included `launchSettings.json` to define launch profiles for HTTP,
    HTTPS, and IIS Express, with the environment set to `Development` and
    Swagger as the default launch URL. Added Swagger for API
    documentation.
    
    This commit sets up the foundation for the gateway application,
    enabling secure and efficient routing between services.

---
+++ FIAPCloudGames.Gateway.Api/Program.cs
@@ -0,0 +1,44 @@
+using System.Security.Claims;
+using System.Text;
+using FIAPCloudGames.Gateway.Api;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)),
+            ClockSkew = TimeSpan.Zero,
+            RoleClaimType = ClaimTypes.Role
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole(Roles.Administrator));
+});
+
+var app = builder.Build();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapReverseProxy();
+
+app.Run();
+
---
+++ FIAPCloudGames.Gateway.Api/Roles.cs
@@ -0,0 +1,7 @@
+﻿namespace FIAPCloudGames.Gateway.Api;
+
+public static class Roles
+{
+    public const string Administrator = "Administrator";
+    public const string User = "User";
+}
